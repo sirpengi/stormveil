99af76189d120e8a504d4c39f49c9f675739765c
Samuel Ferrell <samcferrell@gmail.com>
2019-01-10 16:54:38 -0600

    Preserve move history in game state.

--- src/state.ts
+++ src/state.ts
@@ -11,7 +11,13 @@ interface ISimpleState {
     turn: Side;
 }
 
+interface IMove {
+    a: Vector;
+    b: Vector;
+}
+
 interface IState extends ISimpleState {
+    history: IMove[];
     initial: ISimpleState;
 }
 
@@ -30,6 +36,7 @@ export function createNew(options: IOptions): IState {
     return {
         board: options.board,
         turn: options.start,
+        history: [],
         initial: {
             board: options.board,
             turn: options.start,
@@ -41,5 +48,6 @@ export function play(s: IState, a: Vector, b: Vector): IState {
     const nextState = { ...s };
     nextState.board = resolve(nextState.board, a, b);
     nextState.turn = opponent(nextState.turn);
+    nextState.history = nextState.history.concat({ a, b });
     return nextState;
 }
