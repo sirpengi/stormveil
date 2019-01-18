dcf08b71106fcb3e1fe6514535839759b5373323
Samuel Ferrell <samcferrell@gmail.com>
2019-01-17 19:59:02 -0600

    Update publish script (temp).

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,8 +2,10 @@ npm run prepare
 cd ./packages/stormveil-ui/
 npm run bundle
 tar zcvf $env:temp\stormveil-ui.gz web
+cd ../../
 git checkout gh-pages
+git clean -fd
 tar zxvf $env:temp\stormveil-ui.gz --strip-components=1
-git add --all
-git commit --amend "Publish to GitHub pages."
-git push origin gh-pages --force-with-lease
+git add -u
+# git commit --amend "Publish to GitHub pages."
+# git push origin gh-pages --force-with-lease
