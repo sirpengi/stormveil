17ebbf2edb532ebaf42df69e9cc52bcd078a1244
Samuel Ferrell <samcferrell@gmail.com>
2019-01-17 19:06:34 -0600

    Pass mode via env arg.

--- packages/stormveil-ui/webpack.config.js
+++ packages/stormveil-ui/webpack.config.js
@@ -1,23 +1,36 @@
 const path = require('path');
 
-module.exports = {
-    mode: "development",
-    entry: "./src/main.ts",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"]
-    },
-    output: {
-        path: path.resolve(__dirname, "web/script"),
-        filename: "ui.bundle.js"
+function devtool(mode) {
+    switch (mode) {
+        case "development":
+            return "inline-source-map";
+        case "production":
+            return "source-map";
+        default:
+            return "eval";
     }
+}
+
+module.exports = env => {
+    return {
+        mode: env.mode,
+        entry: "./src/main.ts",
+        devtool: devtool(env.mode),
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: [".js", ".ts", ".tsx"]
+        },
+        output: {
+            path: path.resolve(__dirname, "web/script"),
+            filename: "ui.bundle.js"
+        }
+    };
 };
