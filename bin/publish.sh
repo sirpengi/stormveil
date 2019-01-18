ee728e78b7a1f8f2754bbfed94871ef379074ef8
Samuel Ferrell <samcferrell@gmail.com>
2019-01-18 01:36:19 -0600

    Update publish script.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,11 +1,23 @@
+# This script builds the UI project and pushes a new commit to the gh-pages
+# branch.
+Directory=$HOME/stormveil-gh-pages
+
+# Build the project.
 npm run prepare
-cd ./packages/stormveil-ui/
+cd packages/stormveil-ui
 npm run bundle
-tar zcvf $env:temp\stormveil-ui.gz web
-cd ../../
-git checkout gh-pages
-git clean -fd
-tar zxvf $env:temp\stormveil-ui.gz --strip-components=1
-git add -u
-# git commit --amend "Publish to GitHub pages."
-# git push origin gh-pages --force-with-lease
+
+# # Clone the gh-pages branch into a new, temporary directory to safely copy
+# # the contents over.
+git clone --single-branch --branch gh-pages git@github.com:samcf/stormveil.git $Directory
+cp -r web/. $Directory/
+
+# # Commit and push the changes to gh-pages.
+cd $Directory
+git add --all
+git commit --allow-empty --amend -m "Publish to GitHub pages."
+git push --force-with-lease origin gh-pages
+
+# # Cleanup the temporary directory.
+cd $HOME
+rm -rf $Directory
