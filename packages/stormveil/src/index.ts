f348109786406480894bccadb5d62a0e4c53f248
Samuel Ferrell <samcferrell@gmail.com>
2019-01-21 18:19:42 -0600

    Fix package entry, avoid double imports.

--- packages/stormveil/src/index.ts
+++ packages/stormveil/src/index.ts
@@ -1,7 +1,8 @@
 import { IOptions, Shell } from "./shell";
-export { Shell as State } from "./shell";
 export { Team } from "./team";
 export { Tile } from "./tile";
+
+export type State = Shell;
 
 export function createNew(options: IOptions): Shell {
     return new Shell(options);
